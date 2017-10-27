2bb757416553581e6148898dba9a79f5e19ff991
garfield <shaudai0402@hotmail.com>
2017-10-27 17:40:48 +0800

    send a request to google map api and get the result in json format

---
+++ app.js
@@ -0,0 +1,8 @@
+const request = require('request');
+
+request({
+	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20philadelphia&key=AIzaSyDKrHnCompmZxME-PpqnsTYiZjGEWdg10k',
+	json: true
+}, (error, response, body) => {
+	console.log(body);
+});
