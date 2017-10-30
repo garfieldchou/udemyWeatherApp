896ee163e463940ba11c601c4a3d62e9770f1dfc
garfield <shaudai0402@hotmail.com>
2017-10-30 08:59:31 +0800

    change the url and check the error code error-> code-> ENOTFOUND means that our local machine could not connect to the host provided

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const request = require('request');
 
 request({
-	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20philadelphia&key=AIzaSyDKrHnCompmZxME-PpqnsTYiZjGEWdg10k',
+	url: 'https://mapsgleapis.com/maps/api/geocode/json?address=1301%20lombard%20philadelphia&key=AIzaSyDKrHnCompmZxME-PpqnsTYiZjGEWdg10k',
 	json: true
 }, (error, response, body) => {
-	console.log(JSON.stringify(body, undefined, 2));
+	console.log(JSON.stringify(error, undefined, 2));
 });
