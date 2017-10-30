8f73417ee55b8a489a756df07a05344772c5b7be
garfield <shaudai0402@hotmail.com>
2017-10-30 17:20:52 +0800

    resolve the promise -> fulfilled
    resolve & reject, manage the states of the promise (such as database request or http request)

---
+++ playground/promise.js
@@ -0,0 +1,9 @@
+var somePromise = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve('Hey. It worked!');	
+	}, 2500);
+});
+
+somePromise.then((message) => {
+	console.log('Success: ', message);
+})
