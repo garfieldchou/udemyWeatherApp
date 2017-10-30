6f16d811cff8c00230d087d026ad2f38c8fcf31f
garfield <shaudai0402@hotmail.com>
2017-10-30 14:59:53 +0800

    query the current temperature with static latitude & logitude for test

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
 
-const geocode = require('./geocode/geocode.js');
+// const geocode = require('./geocode/geocode.js');
 
-const argv = yargs
-	.options({
-		a: {
-			demand: true,
-			alias: 'address',
-			describe: 'Address to fetch weather for',
-			string: true
-		}
-	})
-	.help()
-	.alias('help', 'h')
-	.argv;
+// const argv = yargs
+// 	.options({
+// 		a: {
+// 			demand: true,
+// 			alias: 'address',
+// 			describe: 'Address to fetch weather for',
+// 			string: true
+// 		}
+// 	})
+// 	.help()
+// 	.alias('help', 'h')
+// 	.argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-	if (errorMessage) {
-		console.log(errorMessage);
-	} else {
-		console.log(JSON.stringify(results, undefined, 2));
-	}
-});
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+// 	if (errorMessage) {
+// 		console.log(errorMessage);
+// 	} else {
+// 		console.log(JSON.stringify(results, undefined, 2));
+// 	}
+// });
 
 // f974596b02271d0dc0041b49d6c1ef51
 // https://api.darksky.net/forecast/f974596b02271d0dc0041b49d6c1ef51/37.8267,-122.4233
+
+const request = require('request');
+
+request({
+	url: 'https://api.darksky.net/forecast/f974596b02271d0dc0041b49d6c1ef51/37.8267,-122.4233',
+	json: true
+}, (error, response, body) => {
+	console.log(body.currently.temperature);
+});
