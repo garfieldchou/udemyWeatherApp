5df0f03f5123abaf45288b1ce53f5e5622886e48
garfield <shaudai0402@hotmail.com>
2017-10-30 09:20:38 +0800

    print the latitude and longitude extracted from returned JSON result

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const request = require('request');
 
 request({
-	url: 'https://mapsgleapis.com/maps/api/geocode/json?address=1301%20lombard%20philadelphia&key=AIzaSyDKrHnCompmZxME-PpqnsTYiZjGEWdg10k',
+	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20philadelphia&key=AIzaSyDKrHnCompmZxME-PpqnsTYiZjGEWdg10k',
 	json: true
 }, (error, response, body) => {
-	console.log(JSON.stringify(error, undefined, 2));
+	console.log(`Address: ${body.results[0].formatted_address}`);
+	console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+	console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
