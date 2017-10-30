101504f1cc48ace7f91c2db12a8abc1e5e4dfd68
garfield <shaudai0402@hotmail.com>
2017-10-30 13:43:14 +0800

    print address/lat/lng while status returned OK

--- app.js
+++ app.js
@@ -24,8 +24,9 @@ request({
 		console.log('Unable to connect to Google servers.');
 	} else if (body.status === 'ZERO_RESULTS') {
 		console.log('Unable to find that address.');
+	} else if (body.status === 'OK') {
+		console.log(`Address: ${body.results[0].formatted_address}`);
+		console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+		console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 	}
-	console.log(`Address: ${body.results[0].formatted_address}`);
-	console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-	console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
