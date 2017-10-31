dce138aa2dd3eb7403c9cb58f2deac59aa75c58b
garfield <shaudai0402@hotmail.com>
2017-10-31 13:29:18 +0800

    use chained promises to get weather
    $ node app-promise.js -a 000000
    Unable to find that address.
    
    $ node app-promise.js -a 08822
    Flemington, NJ 08822, USA
    It's currently 45.33. It feels like 45.33.
    
    $ node app-promise.js -a 19146
    Philadelphia, PA 19146, USA
    It's currently 47.68. It feels like 46.2.

--- app-promise.js
+++ app-promise.js
@@ -22,7 +22,15 @@ axios.get(geocodeUrl).then((response) => {
 		throw new Error('Unable to find that address.');
 	}
 
-	console.log(response.data);
+	var lat = response.data.results[0].geometry.location.lat;
+	var lng = response.data.results[0].geometry.location.lng;
+	var weatherUrl = `https://api.darksky.net/forecast/f974596b02271d0dc0041b49d6c1ef51/${lat},${lng}`;
+	console.log(response.data.results[0].formatted_address);
+	return axios.get(weatherUrl);
+}).then((response) => {
+	var temperature = response.data.currently.temperature;
+	var apparentTemperature = response.data.currently.apparentTemperature;
+	console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}.`);
 }).catch((e) => {
 	if (e.code === 'ENOTFOUND') {
 		console.log('Unable to connect to API servers.')
