cc025a194d16d940e3588f6006798d11da625b63
garfield <shaudai0402@hotmail.com>
2017-10-31 13:13:24 +0800

    throw new Error() is going to cause all of the code after it to not run and it's going to move right into the air handler down below
    throw new Error()
    This creates and throws an error letting node know that something went wrong.

--- app-promise.js
+++ app-promise.js
@@ -15,12 +15,18 @@ const argv = yargs
 	.argv;
 
 var encodedAddress = encodeURIComponent(argv.address);
-var geocodeUrl = `https://mapsgoogleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyDKrHnCompmZxME-PpqnsTYiZjGEWdg10k`;
+var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyDKrHnCompmZxME-PpqnsTYiZjGEWdg10k`;
 
 axios.get(geocodeUrl).then((response) => {
+	if (response.data.status === 'ZERO_RESULTS') {
+		throw new Error('Unable to find that address.');
+	}
+
 	console.log(response.data);
 }).catch((e) => {
 	if (e.code === 'ENOTFOUND') {
 		console.log('Unable to connect to API servers.')
+	} else {
+		console.log(e.message);
 	}
 });
